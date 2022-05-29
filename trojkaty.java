28476429199e7753da9885c1469331b70ee36cf9
Arkadiusz Sarga <arek1234556@gmail.com>
2022-05-29 14:10:21 +0200

    Długo wyczekiwany Feature: teraz wykrywamy pitagorejskie

--- trojkaty.java
+++ trojkaty.java
@@ -9,6 +9,21 @@ class Trojkaty {
      * @param {float} b - Długość drugiego boku.
      * @param {float} c - Długość trzeciego boku.
      */
+
+/**
+               * Funkcja do sprawdzenia czy trójkąt o podanych bokach to trójkąt prostokątny.
+               * @param {float} a - Długość pierwszego boku.
+               * @param {float} b - Długość drugiego boku.
+               * @param {float} c - Długość trzeciego boku.
+               */
+               public static boolean czyProstokatny(float a, float b, float c){
+                   if (a * a + b * b == c*c) return true;
+                   if (b * b + c * c == a*a) return true;
+                   if (a * a + c * c == b*b) return true;
+                   return false;
+               }
+
+
     public static void jakiTrojkat(float a, float b, float c){
          if (a == b && b == c && a == c) {
              System.out.println("Trójkąt równoboczny"); 
@@ -17,7 +32,15 @@ class Trojkaty {
 	 if ( a==b || b==c || a==c){
 	   System.out.println("Trójkąt równoramienny");
 	 }
+
+	if (czyProstokatny(a, b, c)) {
+            System.out.println("Trójkąt prostokątny");
+	 }
     }
+
+
+
+
     /** Wyświetla ekran pomocy */
     public static void pomoc(){
         System.out.println("Acme INC. (C) 2022");
